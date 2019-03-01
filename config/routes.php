54a42f2dd5b4917f9686cc5523e7b5a01d771a5a
medbrat13 <medbrat1994@gmail.com>
2019-03-02 02:25:42 +0300

    Добавил список маршрутов

---
+++ config/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    '^$' =>
+    [
+        'controller' => 'main',
+        'action'     => 'index'
+    ],
+
+    'register' =>
+    [
+        'controller' => 'register',
+        'action'     => 'index'
+    ]
+];
