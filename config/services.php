b7b05582b79b29e6c472de87917404bee2156b4b
medbrat13 <medbrat1994@gmail.com>
2019-03-20 22:39:21 +0300

    Добавил список сервисов для их автоматической инициализации

---
+++ config/services.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    \StudentsList\Kernel\Services\DBConfigServiceProvider::class,
+    \StudentsList\Kernel\Services\ConnectionServiceProvider::class,
+    \StudentsList\Kernel\Services\RoutesListServiceProvider::class,
+    \StudentsList\Kernel\Services\RouteServiceProvider::class,
+    \StudentsList\Kernel\Services\RouterServiceProvider::class,
+    \StudentsList\Kernel\Services\QueryBuilderServiceProvider::class,
+    \StudentsList\Kernel\Services\StudentsMapperServiceProvider::class,
+    \StudentsList\Kernel\Services\StudentsValidatorServiceProvider::class,
+
+];
